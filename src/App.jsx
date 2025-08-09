eb3c78be8b4b50c1e7193161036c07a6d76681e1
AbhijeetChattar262 <abchattar5503@gmail.com>
2025-08-09 12:16:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sidebar from './Components/Sidebar.jsx'
+import Main from './Components/Main.jsx'
+
+function App() {
+
+
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  )
+}
+
+export default App
